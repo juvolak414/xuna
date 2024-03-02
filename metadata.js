fa518a31b177192a0647057def123e2cacaf615f
juvolak414 <67997975+juvolak414@users.noreply.github.com>
2024-03-02 16:59:09 -0700

    Create metadata.js

---
+++ metadata.js
@@ -0,0 +1,28 @@
+{
+  "name": "Xuna AI",
+  "symbol": "XUNA",
+  "description": "A detailed description of the Xuna AI token.",
+  "image": "https://example.com/path/to/your/logo.png", // Logo URL
+  "attributes": [
+    {
+      "trait_type": "Feature",
+      "value": "Example"
+    }
+  ],
+  "external_url": "https://yourprojectwebsite.com",
+  "properties": {
+    "files": [
+      {
+        "uri": "https://example.com/path/to/your/logo.png",
+        "type": "image/png"
+      }
+    ],
+    "category": "image",
+    "creators": [
+      {
+        "address": "YourSolanaAddressHere",
+        "share": 100
+      }
+    ]
+  }
+}
